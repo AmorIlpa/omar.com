84ded910186d79b09a7b2eeebdfb3cb01477c418
AmorIlpa <155574562+AmorIlpa@users.noreply.github.com>
2024-01-05 00:48:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// const menuBtn = document.querySelector('.menu-button');
+// const menu = document.querySelector('.menu-open');
+
+// menuBtn.addEventListener('click', () =>{
+//   menu.classList.toggle('clicked');
+// });
+const form= document.querySelector('form');
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "omarilpa09@gmail.com",
+        Password : "36E8D32C570FB916702598804B21C81B02FE",
+        To : 'omarilpa09@gmail.com',
+        From : "omarilpa09@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    ); 
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail();
+});
